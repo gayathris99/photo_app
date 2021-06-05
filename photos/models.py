f236492c2bbc36847ba79b74bdb8a620a906faae
gayathris99 <gayathri11499@gmail.com>
2021-06-05 12:08:13 +0530

    templates and models

---
+++ photos/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null = False, blank= False)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete = models.SET_NULL , null =True)
+    image = models.ImageField(null=False,blank=False)
+
+    def __str__(self):
+        return self.description
